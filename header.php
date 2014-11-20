acde3e06ef641251a59f8730eb50b21f70290036
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-11-20 22:02:21 +0900

    11/20更新

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+</head>
+<body>
+
+<!-- header -->
+<div id="header">
+<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+<p id="desc"><?php bloginfo('description'); ?></p>
+</div><!-- header -->
