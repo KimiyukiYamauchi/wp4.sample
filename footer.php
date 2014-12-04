cf79727409c1aea5331679241ef9046a9ca48aca
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-12-04 21:22:58 +0900

    p32まで

--- footer.php
+++ footer.php
@@ -1,2 +1,8 @@
+<!-- footer -->
+<div id="footer">
+<p>Copyright &copy; <?php bloginfo('name'); ?>, All rights reserved.</p>
+</div><!-- footer -->
+
+</div><!-- container -->
 </body>
 </html>
--- header.php
+++ header.php
@@ -3,11 +3,19 @@
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
 <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
+<!-- container -->
+<div id="container">
 
 <!-- header -->
 <div id="header">
 <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 <p id="desc"><?php bloginfo('description'); ?></p>
+
+<div id="subinfo">
+<a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/feed.png" alt="RSS FEED" width="28" height-"28"></a>
+</div>
+
 </div><!-- header -->
