cfbc8d87436b6ef70611463d5a472506c785d183
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-12-04 22:03:19 +0900

    p37まで

--- header.php
+++ header.php
@@ -18,4 +18,12 @@
 <a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/feed.png" alt="RSS FEED" width="28" height-"28"></a>
 </div>
 
+<div id="nav">
+<?php wp_nav_menu(array('theme_location' => 'navigation')); ?>
+</div>
+
+<?php if(get_header_image()): ?>
+<div id="image"><img src="<?php header_image(); ?>" alt="*" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" /></div>
+<?php endif; ?>
+
 </div><!-- header -->
